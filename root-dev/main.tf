94096f93727c0227d6b9b70c39bdccb0e3f59a71
akshaykoli05 <koliakshay05@gmail.com>
2026-03-08 12:16:49 +0530

    nic chnagesd done

--- root-dev/main.tf
+++ root-dev/main.tf
@@ -18,6 +18,33 @@ module "rg" {
   location = "eastus"
 }
 
+resource "azurerm_virtual_network" "vm_vnet" {
+  name                = "catsweb-dev-vnet"
+  location            = module.rg.location
+  resource_group_name = module.rg.rg_name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "vm_subnet" {
+  name                 = "catsweb-dev-subnet"
+  resource_group_name  = module.rg.rg_name
+  virtual_network_name = azurerm_virtual_network.vm_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "vmnic" {
+  name                = "catsweb-dev-nic"
+  location            = module.rg.location
+  resource_group_name = module.rg.rg_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vm_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
 module "vm" {
   source   = "app.terraform.io/akshayInfra/terraform-module-vm/module"
   version  = "1.2.0"
