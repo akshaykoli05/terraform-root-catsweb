bcec5914e11a70c1f03fe4ab82d63fd9cbda042a
akshaykoli05 <koliakshay05@gmail.com>
2026-03-08 10:25:04 +0530

    commiting vm deployment

---
+++ root-dev/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "rg" {
+  source   = "app.terraform.io/<org-name>/rg/azurerm"
+  version  = "1.0.0"
+  rg_name  = "catsweb-dev-rg"
+  location = "eastus"
+}
+
+module "vm" {
+  source   = "app.terraform.io/<org-name>/vm/azurerm"
+  version  = "1.0.0"
+  rg_name  = module.rg.rg_name
+  vm_name  = "catsweb-dev-vm"   # hard-coded
+  vm_size  = "Standard_B2s"    # hard-coded
+  location = "eastus"          # hard-coded
+  os_type  = "Linux"           # hard-coded
+  nic_id   = "some-nic-id"     # placeholder until NIC module is added
+}
