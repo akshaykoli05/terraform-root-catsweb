ecee6ea2cbdeba669b9dd97a757f7821997bdb17
akshaykoli05 <koliakshay05@gmail.com>
2026-03-09 10:01:48 +0530

    deploy vm with new rg

--- root-dev/main.tf
+++ root-dev/main.tf
@@ -12,23 +12,26 @@ provider "azurerm" {
   features {}
 }
 
-# Use existing RG
-data "azurerm_resource_group" "existing" {
-  name = "catsweb-dev-rg-01"
+module "rg" {
+  source   = "app.terraform.io/akshayInfra/terraform-module-rg/module"
+  version = "1.1.0"
+  rg_name  = "catsweb-dev-rg-01"
+  location = "eastus"
 }
+
 
 # Create VNet
 resource "azurerm_virtual_network" "vm_vnet" {
   name                = "catsweb-dev-vnet"
-  location            = data.azurerm_resource_group.existing.location
-  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = module.rg.location
+  resource_group_name = module.rg.rg_name
   address_space       = ["10.0.0.0/16"]
 }
 
 # Create Subnet
 resource "azurerm_subnet" "vm_subnet" {
   name                 = "catsweb-dev-subnet"
-  resource_group_name  = data.azurerm_resource_group.existing.name
+  resource_group_name  = module.rg.rg_name
   virtual_network_name = azurerm_virtual_network.vm_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
@@ -36,8 +39,8 @@ resource "azurerm_subnet" "vm_subnet" {
 # Create NIC
 resource "azurerm_network_interface" "vmnic01" {
   name                = "azwacatsd01-nic"
-  location            = data.azurerm_resource_group.existing.location
-  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = module.rg.location
+  resource_group_name = module.rg.rg_name
 
   ip_configuration {
     name                          = "internal"
@@ -51,8 +54,8 @@ module "vm01" {
   source   = "app.terraform.io/akshayInfra/terraform-module-vm/module"
   version  = "1.4.1"
 
-  rg_name   = data.azurerm_resource_group.existing.name
-  location  = data.azurerm_resource_group.existing.location
+  rg_name   = module.rg.rg_name
+  location  = module.rg.location
   vm_size   = "Standard_B2s"
   nic_id    = azurerm_network_interface.vmnic01.id
   vm_suffix = "01"
