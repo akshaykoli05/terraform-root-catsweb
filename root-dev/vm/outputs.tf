79d3f3d69e42a059ad3e567fd5685d476fa168dc
akshaykoli05 <koliakshay05@gmail.com>
2026-03-13 10:04:34 +0530

    Deleting created vm

---
+++ root-dev/vm/outputs.tf
@@ -0,0 +1,7 @@
+#output "vm_id" {
+ # value = module.vm.vm_id
+#}
+#
+##output "vm_name" {
+ # value = module.vm.vm_name
+#}
---
+++ root-dev/vm/variables.tf
@@ -0,0 +1,15 @@
+#variable "vm_size" {
+#  description = "Size of the VM"
+ # type        = string
+  #default     = "Standard_B2s"
+#}
+
+#variable "nic_id" {
+#  description = "ID of existing NIC"
+#  type        = string
+#}
+
+#variable "vm_suffix" {
+#  description = "Numeric suffix for VM name (e.g., 01)"
+ # type        = string
+#}##
